f931e32ad9df22f707e55261a82d76e7aa14c3f3
dalentri <aydenjmason05@gmail.com>
2025-03-01 18:36:32 -0600

    Established lootboxes, rarities, and items.

---
+++ lootboxSimulator/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace lootboxSimulator
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome to Lootbox Simulator!");
+            
+            while (true)
+            {
+                Menu.DisplayMenu();
+            }
+            
+        }
+    }
+}
